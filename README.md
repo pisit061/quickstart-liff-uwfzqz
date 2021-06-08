b3aada4ea38847fe958313e4596ca1081096c56c
pisit061 <pisit.na@psru.ac.th>
2021-06-08 23:50:16 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quickstart-liff-uwfzqz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/quickstart-liff-uwfzqz)
---
+++ index.js
@@ -0,0 +1,50 @@
+// Import stylesheets
+import './style.css';
+
+// Body element
+const body = document.getElementById('body');
+
+// Button elements
+const btnSend = document.getElementById('btnSend');
+const btnClose = document.getElementById('btnClose');
+const btnShare = document.getElementById('btnShare');
+const btnLogIn = document.getElementById('btnLogIn');
+const btnLogOut = document.getElementById('btnLogOut');
+const btnScanCode = document.getElementById('btnScanCode');
+const btnOpenWindow = document.getElementById('btnOpenWindow');
+
+// Profile elements
+const email = document.getElementById('email');
+const userId = document.getElementById('userId');
+const pictureUrl = document.getElementById('pictureUrl');
+const displayName = document.getElementById('displayName');
+const statusMessage = document.getElementById('statusMessage');
+
+// QR element
+const code = document.getElementById('code');
+const friendShip = document.getElementById('friendShip');
+
+async function main() {
+  // Initialize LIFF app
+  await liff.init({ liffId: '1656065525-eoy7o7Ko' });
+  // Try a LIFF function
+  switch (liff.getOS()) {
+    case 'android':
+      body.style.backgroundColor = '#d1f5d3';
+      break;
+    case 'ios':
+      body.style.backgroundColor = '#eeeeee';
+      break;
+  }
+}
+async function getUserProfile() {
+  async function getUserProfile() {
+    // ...
+    email.innerHTML = '<b>email:</b> ' + liff.getDecodedIDToken().email;
+  }
+}
+main();
+async function main() {
+  // ...
+  getUserProfile();
+}
